082952c9416634809ca28fc06f63a4daecca7d23
milosilo-dev <72990645+milosilo-dev@users.noreply.github.com>
2022-12-10 08:41:43 +0000

    first commit

---
+++ flask/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/projects/")
+def project():
+    x = ""
+    for i in os.listdir("C:\\Users\\Miles Hansell\\Documents\\Python\\Personal Website\\flask\\templates\\projects\\"):
+        list = i.split(".html")
+        x = x + """
+        """ + list[0]
+    return x
+
+@app.route("/projects/<name>")
+def projects(name):
+    return render_template("/projects/" + name + ".html")
+
+if __name__ == "__main__":
+    app.run()  
---
+++ flask/static/script.js
@@ -0,0 +1,73 @@
+const onScreen = [];
+index = 0;
+index2 = 0;
+
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+}
+
+function checkFunctions(y){
+    y = y.toLowerCase();
+    const x = y.split(" ");
+    if (x[0] == "whoareu" || x[0] == "whoareyou"){
+        return "I am a Proggramer from the uk that has made some projects and this website is for showing them";
+    }
+    if (x[0] == "help" || x[0] == "commands"){
+        return `Commands:
+        help,
+        whoareu,
+        projects(project),
+        projectslist`;
+    }
+    if (x[0] == "projects" || x[0] == "project"){
+        window.location.href = window.location.href + "projects/" + x[1];
+    }
+    if (x[0] == "projectlist"){
+        return loadFile("/projects/");
+    }
+}
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+$(document).ready(function() {
+    function displayHandeler() {
+        var x = $("input:text").val();
+        document.getElementById("terminal-content").innerHTML = document.getElementById("terminal-content").innerHTML + "<p id='" + index + "'>"+ "Guest@TheSecretDev:/$" + x + "<p id='r"+ index +"'>" + checkFunctions(x) + "</p>" + "</p>";
+        onScreen.push(index)
+        var divHeight = document.getElementById('content').offsetHeight;
+        if (divHeight >= 816){
+            //location.reload();
+            var targetHeight = document.getElementById(onScreen[onScreen.length - 1]).offsetHeight;
+            for (let i = 0; i < onScreen.length; i++){
+                if (targetHeight <= 0){
+                    break;
+                }
+                console.log(String(onScreen[i] + index2));
+                var lastHeight = document.getElementById(String(onScreen[i] + index2)).clientHeight;
+                var element = document.getElementById(String(onScreen[i] + index2));
+                element.remove();
+                var element = document.getElementById("r" + String(onScreen[i] + index2));
+                console.log("r" + onScreen[i] + index2);
+                element.remove();
+                targetHeight -= lastHeight;
+                index2++;
+            }
+        }
+        index++;
+    };
+    $(".terminal-text").keyup(function(event){
+        if (event.which == 13){
+            displayHandeler();
+        }
+    });
+});
